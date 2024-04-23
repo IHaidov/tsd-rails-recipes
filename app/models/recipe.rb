4a5ce27c2abfe5e7c26cf1208ec879edb82fea87
Andrei Staravoitau <115485108+andreistaravoitau@users.noreply.github.com>
2024-04-23 17:12:21 +0000

    task-5

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,15 @@
 class Recipe < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :ingredients
+  has_rich_text :content
+
+  validate :validate_number_of_ingredients
+
+  private
+
+  def validate_number_of_ingredients
+    if ingredients.size > 3
+      errors.add(:base, "A recipe cannot have more than 3 ingredients")
+    end
+  end
 end
